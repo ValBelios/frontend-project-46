1e11586051e099f246d7fea5cb23745075342cc3
Valery <wer1wer1wer1@mail.ru>
2025-07-01 00:43:44 +0500

    Запуск проверки Hexlet

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>') // <-- ВАЖНО: ожидаем два аргумента
+  .option('-f, --format <type>', 'output format', 'stylish') // дефолтный формат
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
+    console.log(diff);
+  });
 
 program.parse();
