fbd10ca9f89561379ce739201ad025f872dc3175
Valery <wer1wer1wer1@mail.ru>
2025-07-01 23:47:40 +0500

    Запуск проверки Hexlet

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,10 +1,6 @@
-const indent = (depth, replacer = ' ', spacesCount = 4) => {
-  const count = depth * spacesCount - 2;
-  return replacer.repeat(Math.max(count, 0));
-};
-
-const makeIndent = (depth) => indent(depth);
-const makeBracketIndent = (depth) => indent(depth - 1);
+const makeSignIndent = (depth) => ' '.repeat(Math.max(depth * 4 - 2, 0));
+const makeNormalIndent = (depth) => ' '.repeat(depth * 4);
+const makeBracketIndent = (depth) => ' '.repeat((depth - 1) * 4);
 
 const stringify = (value, depth) => {
   if (typeof value !== 'object' || value === null) {
@@ -12,7 +8,7 @@ const stringify = (value, depth) => {
   }
 
   const lines = Object.entries(value).map(
-    ([key, val]) => `${makeIndent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`
+    ([key, val]) => `${makeNormalIndent(depth + 1)}${key}: ${stringify(val, depth + 1)}`
   );
 
   return ['{', ...lines, `${makeBracketIndent(depth + 1)}}`].join('\n');
@@ -27,27 +23,27 @@ const stylish = (tree) => {
 
       switch (type) {
         case 'added':
-          return `${makeIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
+          return `${makeSignIndent(depth)}+ ${key}: ${stringify(value, depth)}`;
         case 'removed':
-          return `${makeIndent(depth)}- ${key}: ${stringify(value, depth)}`;
+          return `${makeSignIndent(depth)}- ${key}: ${stringify(value, depth)}`;
         case 'unchanged':
-          return `${makeIndent(depth)}  ${key}: ${stringify(value, depth)}`;
+          return `${makeNormalIndent(depth)}${key}: ${stringify(value, depth)}`;
         case 'changed':
           return [
-            `${makeIndent(depth)}- ${key}: ${stringify(oldValue, depth)}`,
-            `${makeIndent(depth)}+ ${key}: ${stringify(newValue, depth)}`
+            `${makeSignIndent(depth)}- ${key}: ${stringify(oldValue, depth)}`,
+            `${makeSignIndent(depth)}+ ${key}: ${stringify(newValue, depth)}`
           ].join('\n');
         case 'nested':
-          return `${makeIndent(depth)}  ${key}: ${iter(children, depth + 1)}`;
+          return `${makeNormalIndent(depth)}${key}: ${iter(children, depth + 1)}`;
         default:
-          throw new Error(`Неизвестный тип операции: '${type}'`);
+          throw new Error(`Неизвестный тип: '${type}'`);
       }
     });
 
     return ['{', ...lines, `${makeBracketIndent(depth)}}`].join('\n');
   };
 
-  return iter(tree, 1);
+  return iter(tree, 0);
 };
 
 export default stylish;
